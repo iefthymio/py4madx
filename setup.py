6e93e4be3dcbeb65955358dd1e8b73dade8f8d30
iefthymio <Ilias.Efthymiopoulos@cern.ch>
2020-03-09 11:20:41 +0100

    udpated setup.py and created my_cool_test_method

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='py4madx',
+    version='0.0.1',
+    description='Scripts to use with cpymad for LHC',
+    url='git@github.com:iefthymio/py4madx.git',
+    author='Ilias Efthymiopoulos',
+    author_email='ilias.efthymiopoulos@cern.ch',
+    license='unlicense',
+    packages=['py4madx'],
+    zip_safe=False
+)
