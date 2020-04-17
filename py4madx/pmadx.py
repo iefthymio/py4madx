47754f405cc3d5c25db4fa54ef988b34d45288cd
iefthymio <Ilias.Efthymiopoulos@cern.ch>
2020-04-17 12:33:35 +0200

    update to twiss table conversion

--- py4madx/pmadx.py
+++ py4madx/pmadx.py
@@ -61,8 +61,12 @@ def getMadGlobals(mmad, sletter=''):
 def twiss2df(twisstable):
 	_twdf = pd.DataFrame.from_dict(twisstable, orient='index').transpose()
 	_twdf['elname'] = _twdf['name'].apply(lambda x : x.split(':')[0])
-	_twdf.set_index('elname')
 	return _twdf.set_index('elname')
+
+def table2df(twisstable):
+	_tdf = pd.DataFrame.from_dict(twisstable, orient='index').transpose()
+	_tdf['elname'] = _tdf['name'].apply(lambda x : x.split(':')[0])
+	return _tdf.set_index('elname')
 
 def mergeLHCBeamTwissTables(mmad, twissb1, twissb2):
 	t_b1 = mmad.table[twissb1].dframe()
